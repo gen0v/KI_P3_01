9da4dcd2f189f5dc6f08ad9e5d696b54800ff8a3
gen0v <eugen.b95@googlemail.com>
2020-06-04 21:58:33 +0200

    pl-res working

--- pl.py
+++ pl.py
@@ -18,44 +18,46 @@ class pl:
         return res
             
     def ask(self, alpha):
-        return not self.pl_resolution(self.kb, alpha)
+        return self.pl_resolution(self.kb, alpha)
 
-    # there needs to be a complementary literal so something can be derived
+    def isComplementaryLiteral(self, literal, sentence):
+        nl = self.negate(literal).pop()
+        for l in sentence:
+            if nl in sentence:
+                print("Contradiction")
+                return True
+        return False
+
     def pl_resolve(self,pair):
-        check = False
         print("Pair: " + str(pair))
+        flag = False
         l1 = pair[0].split(",")
         l2 = pair[1].split(",")
-        s = list(set((pair[0] + "," + pair[1]).split(",")))
-        # work with sentence
-        for i1 in l1:
-            for i2 in l2:
-                # print(set(i1) + " == " + self.negate(i2))
-                if(i1 == self.negate(i2).pop()):
-                    s.remove(i1)
-                    s.remove(i2)
-                    check = True
-                    print("Contradiction")
-        # create new result set
-        # print(",".join(s))
-        # if check fails then there was no contradiction and thus
-        # nothing can be derived so we return a duplicate which will
-        # be deleted because of the type set
-        if(not check):
-            tempset = set()
-            tempset.add(pair[0])
-        elif(s != []):
+        s = list(set(l1+l2))
+        print(s)
+        for literal in l1:
+            # if is complementary literal remove it
+            if self.isComplementaryLiteral(literal,l2):
+                # if flag is set then this means second complementary literal
+                # -> discard
+                if flag:
+                    tempset = set()
+                    tempset.add(pair[0])
+                    return tempset
+                s.remove(literal)
+                s.remove(self.negate(literal).pop())
+                flag = True
+        if s == []:
+            tempset = set(" ")
+            print("test")
+            return tempset
+        else:
             s = ",".join(s)
             if s[0] == ",": s=s[1:]
             tempset = set()
             tempset.add(s)
-        else:
-            tempset = set(" ")
-        
-        print(tempset)
-        print("----")
-        return tempset
-    
+            return tempset
+ 
     # inputs:
     # kb = the knowledge base, a sentence in prop. logic
     # alpha = the query, a sentence in prop. logic
@@ -65,7 +67,7 @@ class pl:
         while True:
             for pair in itertools.combinations(clauses,2):
                 resolvents = self.pl_resolve(pair)
-                # print(resolvents)
+                print(resolvents)
                 if " " in resolvents: return True
                 print(resolvents)
                 new = new.union(resolvents)
@@ -75,7 +77,7 @@ class pl:
             clauses = clauses.union(new)
 
 
-kb = {"-B11,P12,P21","-P12,B11","-P21,B11","B11"}
+kb = {"-B11,P12,P21","-P12,B11","-P21,B11","-B11"}
 pl = pl(kb)
 # no pit in 1 2
 a = "-P12"
