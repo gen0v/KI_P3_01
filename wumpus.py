12ce5bf87f711348070f3436fd0e550b698b5bca
gen0v <eugen.b95@googlemail.com>
2020-06-08 18:36:40 +0200

    wumpus

--- wumpus.py
+++ wumpus.py
@@ -3,5 +3,12 @@ import fh_ac_ai_gym
 
 wumpus_env = gym.make('Wumpus-v0')
 cartpole_env = gym.make('Fuzzy-CartPole-v0')
+obs = wumpus_env.reset()
+while True:
+    print(obs)
+    wumpus_env.render()
+    ac = input("Which action?")
+    obs, reward,done, info = wumpus_env.step(int(ac))
+    # print(reward)
+    if done : break
 
-wumpus_env.render()
